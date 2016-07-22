777416d68a2675a5177119eee75eb883f45e88a9
Alexis Covarrubias <Alexis Covarrubias>
2016-07-22 10:43:51 -0700

    updating files to transfer

--- startbootstrap-agency-1.1.0/credentials.js
+++ startbootstrap-agency-1.1.0/credentials.js
@@ -2,14 +2,6 @@ function checkCredentials(){
 	     var userInput = document.getElementById("username").value;
 		 var userPassword = document.getElementById("pwd").value;
 		 if (userInput == "marwizy" && userPassword == "1234") {
-			 window.location.href = "homeo.html";
-		 }
-}
-
-function checkUserPass(){
-	     var userInput = document.getElementById("username1").value;
-		 var userPassword = document.getElementById("pwd1").value;
-		 if (userInput == "samrav" && userPassword == "5678") {
-			 window.location.href = "homef.html";
+			 window.location.href = "aftersigninforphoenix.html";
 		 }
 }
--- startbootstrap-agency-1.1.0/felon.js
+++ startbootstrap-agency-1.1.0/felon.js
@@ -2,6 +2,6 @@ function checkUserPass(){
 	     var userInput = document.getElementById("username").value;
 		 var userPassword = document.getElementById("pwd").value;
 		 if (userInput == "samrav" && userPassword == "5678") {
-			 window.location.href = "homef.html";
+			 window.location.href = "aftersigninforphoenix.html";
 		 }
 }
