833805f7c3c6729f16a6e8e72777ab723af5b0fa
Alexis Covarrubias <Alexis Covarrubias>
2016-07-25 09:33:53 -0700

    updating all files

--- startbootstrap-agency-1.1.0/credentials.js
+++ startbootstrap-agency-1.1.0/credentials.js
@@ -2,7 +2,7 @@ function checkCredentials(){
 	     var userInput = document.getElementById("username").value;
 		 var userPassword = document.getElementById("pwd").value;
 		 if (userInput == "marwizy" && userPassword == "1234") {
-			 window.location.href = "aftersigninforphoenix.html";
+			 window.location.href = "homeo.html";
 		 }
 }
 
--- startbootstrap-agency-1.1.0/felon.js
+++
@@ -1,7 +0,0 @@
-function checkUserPass(){
-	     var userInput = document.getElementById("username").value;
-		 var userPassword = document.getElementById("pwd").value;
-		 if (userInput == "samrav" && userPassword == "5678") {
-			 window.location.href = "aftersigninforphoenix.html";
-		 }
-}
