90b9b65a85f3dac66c029883c7f89178dd36a507
marwizy <mj.haron@yahoo.com>
2016-07-26 13:52:51 -0400

    adding index.html

--- credentials.js
+++ credentials.js
@@ -2,7 +2,7 @@ function checkCredentials(){
 	     var userInput = document.getElementById("username").value;
 		 var userPassword = document.getElementById("pwd").value;
 		 if (userInput == "marwizy" && userPassword == "1234") {
-			 window.location.href = "homeo.html";
+			 window.location.href = "aftersigninforphoenix.html";
 		 }
 }
 
@@ -10,6 +10,6 @@ function checkUserPass(){
 	     var userInput = document.getElementById("username1").value;
 		 var userPassword = document.getElementById("pwd1").value;
 		 if (userInput == "samrav" && userPassword == "5678") {
-			 window.location.href = "homef.html";
+			 window.location.href = "aftersigninforphoenix.html";
 		 }
 }
